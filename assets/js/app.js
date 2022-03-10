0d25cf74041900876a1280128163cdd8b75212b9
root <root@Adexion-Komp.localdomain>
2022-03-10 17:03:56 +0100

    new site version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,44 @@
 import '../styles/app.css';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free';
+import 'slick-carousel'
+import 'jquery';
 
-import './siwtch';
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+(() => {
+    $('.payment').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 0
+                }
+            }
+        ]
+    });
+})();
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\PriceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +11,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(PriceRepository $priceRepository): Response
+    public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-//            'prices' => $priceRepository->findBy([], ['id' => 'ASC'])
-        ]);
+        return $this->render('page/index.html.twig');
     }
 }
