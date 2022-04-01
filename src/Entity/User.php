6ef48ac0eabfdf8ce3917e37fa4076dcc838319b
stacho <staszekk1212@gmail.com>
2022-04-01 18:27:47 +0200

    PR Fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -141,8 +141,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function toggleAdmin() {
-        if(($roles = array_search("ROLE_ADMIN", $this->roles)) !== false){
+    public function toggleAdmin()
+    {
+        if (($roles = array_search("ROLE_ADMIN", $this->roles)) !== false) {
             unset($this->roles[$roles]);
         } else {
             $this->roles[] = "ROLE_ADMIN";
