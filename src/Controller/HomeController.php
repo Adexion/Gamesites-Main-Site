bc9e22b77fc0045a5e0c238d51d9b1e657c3b341
Mateusz Poźniak <m.pozniak@globkurier.pl>
2022-08-08 15:39:58 +0200

    add rodo, privacy policy and rule

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,28 @@ class HomeController extends AbstractController
     {
         return $this->render('client/page/index.html.twig');
     }
+
+    /**
+     * @Route("/rules", name="rules")
+     */
+    public function rules(): Response
+    {
+        return $this->render('client/page/rules.html.twig');
+    }
+
+    /**
+     * @Route("/privacy-policy", name="privacy")
+     */
+    public function privacy(): Response
+    {
+        return $this->render('client/page/privacy.html.twig');
+    }
+
+    /**
+     * @Route("/rodo", name="rodo")
+     */
+    public function rodo(): Response
+    {
+        return $this->render('client/page/rodo.html.twig');
+    }
 }
